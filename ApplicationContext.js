a08eb69309ce5491e487696da4eb4687b18e787a
Marco Steinke <49523050+MarcoSteinke@users.noreply.github.com>
2022-04-19 12:48:10 +0200

    Create ApplicationContext.js

---
+++ ApplicationContext.js
@@ -0,0 +1,46 @@
+// initialize an application context
+const applicationContext = new Map()
+
+// create any class for this example
+class Car {
+  brand
+
+  constructor(brand){ this.brand = brand }
+
+  getBrand() { return this.brand }
+}
+
+// initialize a bean for the 'Car'-type
+let bean = new Car("BMW")
+
+// implement a method, which puts an instance of a bean in the application context by the bean's constructor name
+const createBean = bean => applicationContext.set(bean.constructor.name, bean)
+
+// finally add the bean to the application context
+createBean(new Car("BMW"))
+
+// now methods can be "annotated" for the injection, by adding inject(PARAMETER_TYPE_AS_STRING) to the parameter
+// This is also inspired by Google Guava (and Spring)
+function printCar(car = inject('Car')) {
+  console.log(car)
+}
+
+// The inject method implementation:
+function inject(type) {
+  if(applicationContext.has(type)) return applicationContext.get(type)
+  else throw new Error(`No qualified bean found for the type ${type}`)
+}
+
+// If you now execute the following method call, you will see the parameter is printed:
+printCar(new Car('VW'))
+// => {brand: 'VW'}
+
+// If you now execute the following method call without any parameter, the bean will be injected instead:
+printCar()
+// => {brand: 'BMW'}
+
+// If you annotate a parameter with "inject", which has no configured bean, an error will be thrown:
+inject('ThisTypeDoesNotExist')
+// => VM2254:3 Uncaught Error: No qualified bean found for the type Carf
+//  at inject (<anonymous>:3:14)
+//  at <anonymous>:1:1
